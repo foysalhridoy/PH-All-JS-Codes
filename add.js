41d64f657752f77c29bc15031218d8182645b6ad
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-18 23:59:52 +0600

    hridoy

--- add.js
+++ add.js
@@ -2,3 +2,10 @@ const orangePrice = 20;
 const mangoPrice = 20;
 console.log(orangePrice + mangoPrice);
 
+const firstName = "Hridoy";
+const lastName = "Khan";
+console.log(firstName + " " + lastName);
+
+const numberString = '10';
+console.log(numberString);
+

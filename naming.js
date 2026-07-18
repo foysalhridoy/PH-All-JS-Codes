e3c7e4d9b55b84b7701f7a72f82d5ee432552258
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-18 23:32:13 +0600

    hridoy

---
+++ naming.js
@@ -0,0 +1,11 @@
+let location = "Daffodil Smart City";
+let age = 23;
+let boyosh = 23;
+
+const my phone number = 01700000000;
+const my_phone_number = 01700000000; //snake case
+const myPhoneNumber = 01700000000; //camel case [usually this we'll use in JS]
+const MyPhoneNumber = 01700000000;// pascal case
+
+const p3erson = 6;
+const person = 6;

0a03b86fcb9d662fe48d24b9b88e4266748eb67b
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-19 18:55:59 +0600

    H

---
+++ subs.js
@@ -0,0 +1,11 @@
+console.log(50+24);
+console.log(50-244);
+
+const shirtPrice = 200;
+const pantPrice = 300;
+console.log(pantPrice-shirtPrice);
+
+const orangePricePerKg = 20;
+const mangoQuantityInKg = 2;
+const payableAmount = orangePricePerKg * mangoQuantityInKg;
+console.log(payableAmount);

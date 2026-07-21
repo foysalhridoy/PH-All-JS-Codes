f121da51c8e98ff09e714791f8edce381c2ef1a5
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-21 11:59:01 +0600

    h

---
+++ if.js
@@ -0,0 +1,12 @@
+if (true) {
+    console.log("This will always run.");
+}
+
+if(8>5){
+    console.log("8 is greater than 5");
+}
+
+if(50<=45){
+    console.log("50 is less than or equal to 45");
+}
+

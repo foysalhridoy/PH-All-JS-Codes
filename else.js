a0cedf9b4fa13d1bc22fcd40ea0ab678c4ee2cc2
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-21 19:23:18 +0600

    h

--- else.js
+++ else.js
@@ -6,3 +6,15 @@ if(rickshawFare < 50) {
     console.log("The rickshaw fare is too high");
     console.log("I'm on diet mama, see you later");
 }
+
+const x = 10;
+const y = 20;
+
+if(x!==y){
+    const z = x + y;
+    console.log("Value of z=", z);
+}
+
+else{
+    console.log(" They are not same same");
+}

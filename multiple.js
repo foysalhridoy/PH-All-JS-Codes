a0c1c0eae41f0e0415dd9554efdd064a6953c6a1
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-21 19:38:02 +0600

    h

--- multiple.js
+++ multiple.js
@@ -2,10 +2,17 @@ const salary = 75000;
 const isRich = true;
 const hasCar = true;
 
-if(salary > 50000 && isRich === true && hasCar === true){
-    console.log("Yoy're selected!");
-    console.log("Congratulations");
+if(salary > 50000 && isRich === true && hasCar === true){  // sob conditions true howa lagbei must
+    // console.log("Yoy're selected!");
+    // console.log("Congratulations");
 }
 else{
-    console.log("Sorry Bro! see you next time");
+    //console.log("Sorry Bro! see you next time");
+}
+
+if(salary > 75000 || isRich === true){  // 2 ta condition er 1 ta true holei hobe
+    console.log("You're selected!");
+}
+else{
+    console.log('Bye bye');
 }

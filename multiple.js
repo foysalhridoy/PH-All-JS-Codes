97148601cb234c4364423a7e55633225c5569553
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-21 19:29:55 +0600

    h

---
+++ multiple.js
@@ -0,0 +1,11 @@
+const salary = 25000;
+const isRich = true;
+const hasCar = true;
+
+if(salary > 50000){
+    console.log("Yoy're selected!");
+    console.log("Congratulations");
+}
+else{
+    console.log("Sorry Bro! see you next time");
+}

637e3380707963c3e417404352946044e6f2372d
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-24 00:15:02 +0600

    h

---
+++ includes.js
@@ -0,0 +1,9 @@
+// const numbers = [10, 11, 12, 13, 14];
+
+// const hasItem = numbers.includes(10)
+// console.log(hasItem)
+
+const numbers = [10, 11, 12, 13, 14];
+
+const hasItem = numbers.includes(600)
+console.log(hasItem)

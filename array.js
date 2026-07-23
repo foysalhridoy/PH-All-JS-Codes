ddf5652c495d6029c23b487d915d3c30c6d275ac
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-23 23:54:56 +0600

    h

---
+++ array.js
@@ -0,0 +1,10 @@
+const player1 = 45; //variable can hold only one value or one data
+const player2 = 98;
+const player3 = 56;
+const player3 = 56;
+const player3 = 56;
+const player3 = 56;
+
+//array can hold more than one value or data
+const team = [123, 89,45,69,78]
+const players = ['Hridoy', 'Foysal', 'Ferdous']

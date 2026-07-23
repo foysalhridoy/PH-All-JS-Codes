36fe9620ce5c3bd2a7ba6c0c1f548e18aa5fa895
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-23 23:28:55 +0600

    h

---
+++ repeat.js
@@ -0,0 +1,19 @@
+// console.log("Show me something")
+// console.log("Show me something")
+// console.log("Show me something")
+// console.log("Show me something")
+// console.log("Show me something")
+
+// const count = 0;
+// let count = 0;
+// count < 10;
+// count++;
+// console.log("do something");
+
+// for(initialization ; condition ; modification){
+//     repetitive task
+// }
+
+for(let count = 0; count <10; count++){
+    console.log('do something');
+}

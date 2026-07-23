4201bc629e702ecdba284ecb801b104bb4341850
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-23 23:45:31 +0600

    h

--- while.js
+++ while.js
@@ -1,3 +1,9 @@
-for(let i = 899; i < 903; i++){
-    console.log('for loop')
+// for(let i = 899; i < 903; i++){
+//     console.log('for loop')
+// }
+
+let i = 899;
+while (i < 903){
+    console.log('inside while loop', i);
+    i++;
 }

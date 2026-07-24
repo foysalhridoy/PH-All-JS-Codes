85689a94b2dfebac63b830cc8d0d4eb8a95cbfdb
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-24 13:26:27 +0600

    h

--- reverse.js
+++ reverse.js
@@ -1,9 +1,16 @@
 const numbers = [87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
 
 const reversedNumbers = numbers.reverse();
-console.log(reversedNumbers);
+// console.log(reversedNumbers);
 
 const friends = ["Abul", "Babul", "Cabul", "Dabul"];
 
 const reversedFriends = friends.reverse();
-console.log(reversedFriends);
+// console.log(reversedFriends);
+
+const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for(let i = 0; i < digits.length; i++){
+    const element = digits[i];
+    console.log(element);
+}

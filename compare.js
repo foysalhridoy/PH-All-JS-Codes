8ce946a42f374873e9617a02894f699031bfb6e5
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-24 13:59:43 +0600

    h

--- compare.js
+++ compare.js
@@ -1,9 +1,20 @@
-const subject = "bangla";
-const book = "Bangla";
+// const subject = "bangla";
+// const book = "BANGLa";
 
-if(subject === book){
-    console.log("yes");
+// console.log(book.toLowerCase);
+// if(subject === book){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+const emailUserProvided = 'hridoy@Mia.com';
+const emailStoredInDatabase = 'hridoy@MIA.com';
+
+if(emailUserProvided.toLowerCase() === emailStoredInDatabase.toLowerCase()){
+    console.log('user can log in');
 }
 else{
-    console.log("no");
+    console.log('user can not log in');
 }

833dfa79e88a564d403a308f197442417e759943
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-24 14:12:59 +0600

    h

--- split.js
+++ split.js
@@ -1,3 +1,6 @@
 const companiesStr = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
 const companies = companiesStr.split(' ');
 console.log(companies);
+
+const againStr = companies.join(' ');
+console.log(againStr);

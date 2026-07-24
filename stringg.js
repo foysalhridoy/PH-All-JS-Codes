62fd6c7d92fbbe63364f4211017b162a4a6a1e2f
foysalhridoy <ffoysalhridoy@gmail.com>
2026-07-24 13:52:41 +0600

    h

---
+++ stringg.js
@@ -0,0 +1,7 @@
+const cup = 'World Cup';
+const tea = 'Tea cup';
+const location = 'Dhaka';
+const vehicle = new string ('Car');
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+console.log(numbers.length);
